ae2f6e41fe0d35186169adf2d4acaffab52ad231
dotscha <dotsch@gmail.com>
2018-07-31 17:05:03 +0200

    it works!

--- kmeans.r
+++ kmeans.r
@@ -1,12 +1,16 @@
-pic = image_read("pic.jpg")
+library(magick)
+
+img = "pic.jpg"  #"hqdefault.jpg"
+xs = 8
+ys = 8
+cset = 256
+
+pic = image_read(img)
 pic = image_scale(pic,c(320,200))
 pic = image_convert(pic,colorspace="gray")
 
-xs = 8
-ys = 8
-
 data = pic[[1]]
-mdata = c()
+mdata = NULL
 for (x in seq(1,320,xs))
 {
   for (y in seq(1,200,ys))
@@ -14,19 +18,21 @@ for (x in seq(1,320,xs))
     mdata = c(mdata, as.integer(data[,x:(x+xs-1),y:(y+ys-1)]))
   }
 }
-m = matrix(mdata,ncol=xs*ys)
+m = matrix(mdata,nrow=xs*ys)
 
-cl = kmeans(m,256,iter.max = 1000)
+cl = kmeans(t(m),cset,algorithm="Hartigan-Wong",iter.max = 1000)
 
 i = 1
 for (x in seq(1,320,xs))
 {
   for (y in seq(1,200,ys))
   {
-    cluster = cl$cluster[i]
-    data[,x:(x+xs-1),y:(y+ys-1)] = as.raw(cl$centers[cluster,])
+    clust = cl$cluster[i]
+    data[,x:(x+xs-1),y:(y+ys-1)] = as.raw(cl$centers[clust,])
     i = i + 1
   }
 }
 
 pic2 = image_read(data)
+
+image_write(pic2,"pic2.png","PNG")
